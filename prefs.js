8c36f238de60a89e71050880b4c946a091bf3750
Rick Opper <rick@localhost.localdomain>
2023-04-13 13:18:58 +0200

    WIP - improve prefs handling and display

--- prefs.js
+++ prefs.js
@@ -10,35 +10,108 @@ function init() {
 }
 
 function fillPreferencesWindow(window) {
-    // Use the same GSettings schema as in `extension.js`
-    const settings = ExtensionUtils.getSettings(
-        'org.gnome.shell.extensions.quicktext');
-    
-    // Create a preferences page and group
-    const page = new Adw.PreferencesPage();
-    const group = new Adw.PreferencesGroup();
-    page.add(group);
+  // Use the same GSettings schema as in `extension.js`
+  const settings = ExtensionUtils.getSettings(
+    'org.gnome.shell.extensions.quicktext');
 
-    // Create a new preferences row
-    const row = new Adw.ActionRow({ title: 'Only one-line entry' });
-    group.add(row);
+  // Create a preferences page and group
+  const page = new Adw.PreferencesPage();
+  const group = new Adw.PreferencesGroup();
+  page.add(group);
 
-    // Create the switch and bind its value to the `show-indicator` key
-    const toggle = new Gtk.Switch({
-        active: settings.get_boolean('quick-multiline'),
-        valign: Gtk.Align.CENTER,
-    });
-    settings.bind(
-        'quick-multiline',
-        toggle,
-        'active',
-        Gio.SettingsBindFlags.DEFAULT
-    );
+  // Create a new preferences row
+  const oneLine = new Adw.ActionRow({ title: 'Only one-line entry' });
+  group.add(oneLine);
+  // Create the switch and bind its value to the `show-indicator` key
+  const toggle = new Gtk.Switch({
+    active: settings.get_boolean('quick-multiline'),
+    valign: Gtk.Align.CENTER,
+  });
+  settings.bind(
+    'quick-multiline',
+    toggle,
+    'active',
+    Gio.SettingsBindFlags.DEFAULT
+  );
 
-    // Add the switch to the row
-    row.add_suffix(toggle);
-    row.activatable_widget = toggle;
+  // Add the switch to the row
+  oneLine.add_suffix(toggle);
+  oneLine.activatable_widget = toggle;
 
-    // Add our page to the window
-    window.add(page);
+
+  // Create a new preferences row
+  const filePath = new Adw.ActionRow({ title: 'Path to destination note file' });
+  group.add(filePath);
+
+  const filePathEntry = new Gtk.Entry({
+    text: settings.get_string('quick-filepath'),
+    valign: Gtk.Align.CENTER,
+  });
+  settings.bind(
+    'quick-filepath',
+    filePathEntry,
+    'text',
+    Gio.SettingsBindFlags.DEFAULT
+  );
+
+  // Add the switch to the row
+  filePath.add_suffix(filePathEntry);
+  filePath.activatable_widget = filePathEntry;
+
+  // Create a new preferences row
+  const prefix = new Adw.ActionRow({ title: 'Prefix line for new notes (leave blank for current date and time)' });
+  group.add(prefix);
+
+  const prefixEntry = new Gtk.Entry({
+    text: settings.get_string('quick-prepend'),
+    valign: Gtk.Align.CENTER,
+  });
+  settings.bind(
+    'quick-prepend',
+    prefixEntry,
+    'text',
+    Gio.SettingsBindFlags.DEFAULT
+  );
+
+  // Add the switch to the row
+  prefix.add_suffix(prefixEntry);
+  prefix.activatable_widget = prefixEntry;
+
+  // Create a new preferences row
+  const suffix = new Adw.ActionRow({ title: 'Suffix line for new notes' });
+  group.add(suffix);
+
+  const suffixEntry = new Gtk.Entry({
+    text: settings.get_string('quick-append'),
+    valign: Gtk.Align.CENTER,
+  });
+  settings.bind(
+    'quick-append',
+    suffixEntry,
+    'text',
+    Gio.SettingsBindFlags.DEFAULT
+  );
+
+  // Add the switch to the row
+  suffix.add_suffix(suffixEntry);
+  suffix.activatable_widget = suffixEntry;
+
+  // Create a new preferences row
+  const position = new Adw.ActionRow({ title: 'Where to insert new notes in the file' });
+  group.add(position);
+
+  const positionEntry = Gtk.DropDown.new_from_strings(['BEG','END']);
+  settings.bind(
+    'quick-pendlocation',
+    positionEntry,
+    'selected-item',
+    Gio.SettingsBindFlags.DEFAULT
+  );
+
+  // Add the switch to the row
+  position.add_suffix(positionEntry);
+  position.activatable_widget = positionEntry;
+
+  // Add our page to the window
+  window.add(page);
 }
